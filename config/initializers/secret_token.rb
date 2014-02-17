4b7027f2aa54d9fa2afdfbc2976ddc2289c9e64f
Karen Kirr <kkirr77@yahoo.com>
2014-02-16 21:02:39 -0800

    add app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseAuth::Application.config.secret_key_base = '54ce171d84d3c6249a62f675fca06f477af2e5ca752e566be527ed850b0e11f3519c74b9d515b8b2e7d8b67cf0d086c1da94a6b20eb005183543f578ebb28b21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseAuth::Application.config.session_store :cookie_store, key: '_DeviseAuth_session'
